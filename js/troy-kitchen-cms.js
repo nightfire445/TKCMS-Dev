0263c134aa79b24193bda68cc9cb6ed9c024c0b6
Yarden <yardenn1@gmail.com>
2017-10-25 14:38:36 -0400

    Completed front-end prototype

--- js/troy-kitchen-cms.js
+++ js/troy-kitchen-cms.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   //Static for prototype
   displayVendors(1);
   displayVendors(0);
-
+  populateLocation();
   //Dropdown content needs to be dynamically generated
   //Probably change it to custom dropdown because this sux
   //var dropdown = "<div class='dropdown btn-group'><button class='btn btn-primary dropdown-toggle' type='button' id='dropdownMenu2' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Change vendor</button><div class='dropdown-menu' aria-labelledby='dropdownMenu2'><button class='dropdown-item' type='button' onclick='changeVendor(event)'>Vendor 1</button><button class='dropdown-item' type='button' onclick='changeVendor(event)'>Vendor 2</button><button class='dropdown-item' type='button' onclick='changeVendor(event)'>Vendor 3</button><button class='dropdown-item' type='button' onclick='changeVendor(event)'>Vendor 4</button><button class='dropdown-item' type='button' onclick='changeVendor(event)'>Vendor 5</button><div class='dropdown-divider'></div><button class='dropdown-item' type='button' >Clear Vendor</button></div></div>";
@@ -13,11 +13,11 @@ $(document).ready(function() {
 function displayVendors(state) {
   var vendors = 5;
   var generate_vendors = "";
-  var buttons = "<div class='btn_container'><button class='btn btn-primary' data-toggle='modal' data-target='#edit_vendor'>Edit Vendor</button><br/>";
+  var buttons = "<div class='btn_container'><button type='button' class='btn btn-default' data-toggle='modal' data-target='#edit_vendor'>Edit Vendor</button><br/>";
   if(state == 1)
-    buttons += "<button class='btn btn-primary'>Deactivate</button></div>"
+    buttons += "<button  type='button' class='btn btn-danger btn-xs'>Deactivate</button></div>"
   else
-    buttons += "<button class='btn btn-primary'>Activate</button></div>"
+    buttons += "<button type='button' class='btn btn-success btn-xs'>Activate</button></div>"
   for(var i = 0; i < vendors; i++) {
     generate_vendors += "<div class='vendor'><div class='vendor_name'>Vendor " + (i+1) + buttons + "</div></div>";
 
@@ -27,4 +27,19 @@ function displayVendors(state) {
     $("#active_vendors_container").html(generate_vendors);
   else
     $("#inactive_vendors_container").html(generate_vendors);
+}
+
+function populateLocation() {
+  var output = "";
+  var locations = ["Troy", "Schenectady"];
+  for(var i = 0; i < locations.length; i++) {
+    output += "<button class='dropdown-item' type='button' onclick='updateLocation(event)'>" + locations[i] + "</button>"
+  }
+  $("#dropdownMenu").html(output);
+}
+
+function updateLocation(event) {
+  $("#location").html("Current Location: " + event.currentTarget.innerText);
+  displayVendors(1);
+  displayVendors(0);
 }
