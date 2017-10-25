1843a9a9de984152ba1e5514f7f28138e7c839a7
Solomon <Moris@rpi.edu>
2017-10-25 18:29:32 -0400

    changing the relative url again

--- php/view/dashboard.php
+++ php/view/dashboard.php
@@ -1,5 +1,5 @@
 <?php
 $doc = new DOMDocument();
-$doc->loadHTMLFile(__DIR__."/../../html/troy-kitchen-cms.html");
+$doc->loadHTMLFile("../../html/troy-kitchen-cms.html");
 echo $doc->saveHTML();
 ?>
