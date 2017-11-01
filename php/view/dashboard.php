3ce6acebb8aeeef93db3fe152a24d68227079b3b
Solomon <Moris@rpi.edu>
2017-11-01 10:55:27 -0400

    trying to pull from the server variable, the document_root was empty

--- php/view/dashboard.php
+++ php/view/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 $doc = new DOMDocument();
-echo $DOCUMENT_ROOT;
-$doc->loadHTMLFile( $DOCUMENT_ROOT."html/troy-kitchen-cms.html");
+echo $_SERVER[$DOCUMENT_ROOT];
+$doc->loadHTMLFile( $_SERVER[$DOCUMENT_ROOT]."html/troy-kitchen-cms.html");
 echo $doc->saveHTML();
 ?>
