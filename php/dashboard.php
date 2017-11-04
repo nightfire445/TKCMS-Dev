01e0e788664eeeafa23035d4fb0e37a2867470d0
Solomon <Moris@rpi.edu>
2017-11-04 16:22:54 -0400

    attempting to fix the error with loading the template via the relative link in the model/view; doing so with the file location fix (dirname(_FILE)

--- php/dashboard.php
+++ php/dashboard.php
@@ -3,7 +3,7 @@ require "./models/dashboard_model.php";
 require "./views/dashboard_view.php";
 require "./controllers/dashboard_controller.php";
 $model = new Model();
-$model->loadTemplateFile("./../../html/troy-kitchen-cms.html");
+$model->loadTemplateFile(dirname(__FILE__). "/../../html/troy-kitchen-cms.html");
 $controller = new Controller($model);
 $view = new View($controller, $model);
 $view->output();
