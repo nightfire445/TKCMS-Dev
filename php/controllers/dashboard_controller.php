3c3705e3aedadd8a6eaa9a70dfb74e0c805ac38c
Solomon <Moris@rpi.edu>
2017-11-05 18:56:05 -0500

    modified troy-kitchen-cms to call the controller function add_vendor, move the logic to add the vendor into add_vendor in the controller, along with some debug statements for debugging

--- php/controllers/dashboard_controller.php
+++ php/controllers/dashboard_controller.php
@@ -9,22 +9,23 @@
 
 	    public function add_vendor(){
 
+		    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+		        echo "<script>alert('1')</script>";
+		    	if(isset($_POST["add_vendor"])){
+
+		    		if(  isset($_POST["vendor_name"]) && isset($_POST["description"])  ){
+		    			$this->model->store_vendor();
+		    			echo "<script>alert('vendor stored')</script>";
+
+		    		}
+
+		    	}
+		    	
+		        //edit_vendor
+		    }
+
 	    }
 
-
-	    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-
-	        echo "<script>alert(`1`)</script>";
-	    	if(isset($_POST["add_vendor"])){
-
-	    		if(  isset($_POST["vendor_name"]) && isset($_POST["description"])  ){
-	    			$this->model->store_vendor();
-
-	    		}
-
-	    	}
-	    	
-	        //edit_vendor
-	    }
 	}
 ?>
