cc5c27b98d074cb1d549415fcb162db7308c3b98
Solomon <Moris@rpi.edu>
2017-11-05 17:20:50 -0500

    added logic to the controller to call the model's store_vendor function if the add_vendor form was posted

--- php/models/dashboard_model.php
+++ php/models/dashboard_model.php
@@ -24,8 +24,47 @@ class Model
     	return;
     }
 
-    public function uploadImage(){
-    	return;
+    public function uploadImgur(){
+        
+
+        return;
     }
+
+    public function storeVendor(){
+
+        if(isset($_POST["add_vendor"])){
+
+            //We need the vendor stored before we can store the images due to vendor id being a foriegn key 
+
+            $insert_vendor = $this->prepare("INSERT INTO `vendor` (`name`, `description`, `location`, `deployed` ) VALUES (:name, :description, :location, :deployed) ");
+
+            $insert_vendor->execute(array(':name' => $_POST["vendor-name"], ':description' => $_POST["description"], ':location' => $_POST["location"], ':deployed' => 0 ));
+
+
+            //images may or may not be included in adding the vendor.
+
+            if(isset($_POST["images"])){
+                $image_urls = [];
+                foreach ($_POST["images"] as $value) {
+                    $imgur_url = $this->model->uploadImgur($value);
+                    $image_urls[] = $imgur_url;
+                }
+                
+            }
+
+            if(isset($_POST["menu"]) ){
+               $menu_url = $this->model->uploadImgur($_POST["menu"]);
+
+            }
+
+            //TO DO: store the images and or menu in the db
+
+
+        }
+
+
+        return;
+    }
+
 }
 ?>
