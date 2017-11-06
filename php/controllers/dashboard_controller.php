196d6cb074076210ae32e88dc372573b2f3a8672
Solomon <Moris@rpi.edu>
2017-11-05 19:35:30 -0500

    removing all the php html stuff in favor of posting to dashboard.php, moved the logic there to call the controller method add_vendor if its posted

--- php/controllers/dashboard_controller.php
+++ php/controllers/dashboard_controller.php
@@ -8,22 +8,12 @@
 	    }
 
 	    public function add_vendor(){
-	    	echo "<script>alert('1')</script>";
-		    if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-		        echo "<script>alert('1')</script>";
-		    	if(isset($_POST["add_vendor"])){
+	    	if(  isset($_POST["vendor_name"]) && isset($_POST["description"])  ){
+				$this->model->store_vendor();
+				echo "<script>alert('vendor stored')</script>";
 
-		    		if(  isset($_POST["vendor_name"]) && isset($_POST["description"])  ){
-		    			$this->model->store_vendor();
-		    			echo "<script>alert('vendor stored')</script>";
-
-		    		}
-
-		    	}
-		    	
-		        //edit_vendor
-		    }
+			}
 
 	    }
 
--- php/dashboard.php
+++ php/dashboard.php
@@ -1,11 +1,17 @@
 <?php
-require "./models/dashboard_model.php";
-require "./views/dashboard_view.php";
-require "./controllers/dashboard_controller.php";
-$model = new Model();
-$model->loadTemplateFile(dirname(__FILE__). "/../html/troy-kitchen-cms.html");
-$controller = new Controller($model);
-$view = new View($controller, $model);
-$view->output();
-
+	require "./models/dashboard_model.php";
+	require "./views/dashboard_view.php";
+	require "./controllers/dashboard_controller.php";
+	$model = new Model();
+	$model->loadTemplateFile(dirname(__FILE__). "/../html/troy-kitchen-cms.html");
+	$controller = new Controller($model);
+	$view = new View($controller, $model);
+	$view->output();
+	if($_SERVER['REQUEST_METHOD'] === 'POST'){
+		if(isset($_POST["add_vendor"])){
+			$controller->add_vendor();
+		}
+		
+	    //edit_vendor
+	}
 ?>
