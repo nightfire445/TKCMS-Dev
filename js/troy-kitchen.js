4da4e94d77460a1009e67246fe79217031d34b38
Dan Westover <fun2not@gmail.com>
2017-11-08 21:04:15 -0500

    Added hours template page, updated main page

--- js/troy-kitchen.js
+++ js/troy-kitchen.js
@@ -1,7 +1,9 @@
 //code for hover of vendors
 $(document).ready( function() {
+
   $('.stall').on('mouseenter', function() {
       desc = $(this).attr('data-desc');
       $('#content').text(desc);
   });
+
 });
