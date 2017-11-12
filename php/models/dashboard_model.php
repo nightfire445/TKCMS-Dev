1b34bdee92a09213dbd8ffb22bc224cb9dc7ec73
Solomon <Moris@rpi.edu>
2017-11-12 17:31:48 -0500

    reverting to predebug state

--- php/models/dashboard_model.php
+++ php/models/dashboard_model.php
@@ -37,8 +37,7 @@ class Model
             $this->dbconn->beginTransaction();
             $insert_vendor = $this->dbconn->prepare("INSERT INTO `vendor` (`name`, `description`, `location`, `deployed` ) VALUES (:name, :description, :location, :deployed) ");
 
-            $status = $insert_vendor->execute(array(':name' => $_POST["vendor_name"], ':description' => $_POST["description"], ':location' => 0, ':deployed' => 'FALSE' ));
-
+            $status = $insert_vendor->execute(array(':name' => $_POST["vendor_name"], ':description' => $_POST["description"], ':location' => 0, ':deployed' => 0));
 
             //images may or may not be included in adding the vendor.
             if(isset($_POST["images"])){
