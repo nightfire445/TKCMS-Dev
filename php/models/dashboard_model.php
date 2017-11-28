3ab2efcbd23260ec154359f6a49ca6060234a198
Solomon <Moris@rpi.edu>
2017-11-28 17:07:12 -0500

    previous method has an undefined index error, changed to using array_push;

--- php/models/dashboard_model.php
+++ php/models/dashboard_model.php
@@ -43,7 +43,7 @@ class Model
 
         //we need to get the images associated with each vendor
         foreach ($vendors as $key => $value) {
-             $vendors[$key]["images"] = $this->loadVendorImages($vendors['vendor_id']);
+            array_push( $vendors[$key]["images"], $this->loadVendorImages($vendors['vendor_id']) );
         }
 
     	return $vendors;
