00dde9d943e49f354d3177c681a788fe96af4a15
Yarden <yardenn1@gmail.com>
2017-11-28 19:44:17 -0500

    Added JS for image and menu rendering

--- js/troy-kitchen-cms.js
+++ js/troy-kitchen-cms.js
@@ -58,6 +58,19 @@ function populateInfo(event, vendor_data) {
     insert_image = "<img src='../resources/" + logo + "' alt='Could not load " + logo + "' class='logo'>";
   }
   $("#existing_logo").html(insert_image);
+  var images = vendor_data.images;
+  var image_string = "";
+  for(var i = 0; i < images.length; i++) {
+    image_string += "<div class='image-container'><img src='../resources/" + images[i].images_url + "' alt='Could not load " + images[i].images_url + "' class='images'>";
+    image_string += "<button class='remove-image'>x</button></div>";
+  }
+  $("#existing_images").html(image_string);
+  var menu = vendor_data.menu_url;
+  var insert_menu = 'No Menu Available';
+  if(menu != "") {
+    insert_menu = "<img src='../resources/" + menu + "' alt='Could not load " + menu + "' class='menu'>";
+  }
+  $("#existing_menu").html(insert_menu);
   //TESTING IMAGE
 //  var logo = "https://cdn.bulbagarden.net/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png";
 //  if(logo != null) {
