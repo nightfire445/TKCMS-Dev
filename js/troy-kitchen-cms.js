1b8850b78dd1866ffb9a95a3f06afa76c20954c0
Solomon <Moris@rpi.edu>
2017-11-29 13:42:12 -0500

    forgot closing "

--- js/troy-kitchen-cms.js
+++ js/troy-kitchen-cms.js
@@ -46,7 +46,7 @@ function populateInfo(event, vendor_data) {
   var image_string = "";
   for(var i = 0; i < images.length; i++) {
     image_string += "<div id='image_"+ i +"' class='image-container'><img src='../resources/" + images[i].image_url + "' alt='" + images[i].image_url + "' class='images'>";
-    image_string += "<button type='button' class='btn btn-danger delete-img' onclick='$.post( \"dashboard.php\", { delete_image: \"\", image_url: \"" + images[i].image_url + "\" } ); $(\"image_"+ i +").attr(\"hidden\", \"hidden\"); '>Delete</button></div>";
+    image_string += "<button type='button' class='btn btn-danger delete-img' onclick='$.post( \"dashboard.php\", { delete_image: \"\", image_url: \"" + images[i].image_url + "\" } ); $(\"image_"+ i +"\").attr(\"hidden\", \"hidden\"); '>Delete</button></div>";
   }
   $("#existing_images").html(image_string);
   var menu = vendor_data.menu_url;
