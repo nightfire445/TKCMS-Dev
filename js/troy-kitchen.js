d638cbe1097b5585f6cf054537db81e65e6b2977
Yarden <yardenn1@gmail.com>
2017-11-29 16:34:02 -0500

    Updated alt text and updated styling for logo icons

--- js/troy-kitchen.js
+++ js/troy-kitchen.js
@@ -18,14 +18,14 @@ function populateContent(vendor_data) {
   var menu = vendor_data.menu_url;
   var insert_menu = "<p>No Menu Available for " + vendor_data.name + "</p>";
   if(menu != "" & menu != null) {
-    insert_menu = "<div id='menu_container'><img src='../resources/" + menu + "' alt='" + menu + "' class='menu'></div>";
+    insert_menu = "<div id='menu_container'><img src='../resources/" + menu + "' alt='Unable to load vendor menu' class='menu'></div>";
   }
   output += insert_menu;
   var images = vendor_data.images;
   output += "<div id='images_container'>";
   var image_string = "";
   for(var i = 0; i < images.length; i++) {
-    image_string += "<div class='image-container'><img src='../resources/" + images[i].image_url + "' alt='" + images[i].image_url + "' class='images'>";
+    image_string += "<div class='image-container'><img src='../resources/" + images[i].image_url + "' alt='Unable to load vendor image' class='images'>";
     image_string += "</div>";
   }
   output += image_string + "</div></div>";
