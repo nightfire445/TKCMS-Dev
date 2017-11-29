18fe7b85ddea0b030722b9f1b3f8c29926662279
Yarden <yardenn1@gmail.com>
2017-11-29 14:45:09 -0500

    dynamically displaying vendor info

--- js/troy-kitchen.js
+++ js/troy-kitchen.js
@@ -9,5 +9,24 @@ $(document).ready( function() {
 });
 
 function populateContent(vendor_data) {
-  $("#content").text(vendor_data.name);
+  console.log(vendor_data);
+  output = "";
+  output += "<h2 class='vendor_name'>" + vendor_data.name + "</h2>";
+  output += "<h3 class='vendor_desc'>" + vendor_data.description + "</h3>";
+  output += "<div id='images_container'>";
+  
+  var menu = vendor_data.menu_url;
+  var insert_menu = "<p>No Menu Available for " + vendor_data.name + "</p>";
+  if(menu != "" & menu != null) {
+    insert_menu = "<div id='menu_container'><img src='../resources/" + menu + "' alt='" + menu + "' class='menu'></div>";
+  }
+  output += insert_menu;
+  var images = vendor_data.images;
+  var image_string = "";
+  for(var i = 0; i < images.length; i++) {
+    image_string += "<div class='image-container'><img src='../resources/" + images[i].image_url + "' alt='" + images[i].image_url + "' class='images'>";
+    image_string += "</div>";
+  }
+  output += image_string + "</div>";
+  $("#content").html(output);
 }
