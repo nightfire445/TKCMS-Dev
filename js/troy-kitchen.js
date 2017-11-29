b814912b49b443a17233253251aaa200d1ba0fcb
Yarden <yardenn1@gmail.com>
2017-11-29 14:25:22 -0500

    Working on populating vendor info into the webpage

--- js/troy-kitchen.js
+++ js/troy-kitchen.js
@@ -1,9 +1,13 @@
 //code for hover of vendors
 $(document).ready( function() {
 
-  $('.stall').on('mouseenter', function() {
-      desc = $(this).attr('data-desc');
-      $('#content').text(desc);
-  });
+//  $('.stall').on('mouseenter', function() {
+//      desc = $(this).attr('data-desc');
+//      $('#content').text(desc.name);
+//  });
 
 });
+
+function populateContent(vendor_data) {
+  $("#content").text(vendor_data.name);
+}
