934ad30d181d71cd1364c87f3a0dd53deb607727
Yarden <yardenn1@gmail.com>
2017-11-29 15:49:26 -0500

    CSS tweaks

--- js/troy-kitchen.js
+++ js/troy-kitchen.js
@@ -13,7 +13,7 @@ function populateContent(vendor_data) {
   output = "";
   output += "<h2 class='vendor_name'>" + vendor_data.name + "</h2>";
   output += "<h3 class='vendor_desc'>" + vendor_data.description + "</h3>";
-  output += "<div id='images_container'>";
+  output += "<div id='content_container'>";
   
   var menu = vendor_data.menu_url;
   var insert_menu = "<p>No Menu Available for " + vendor_data.name + "</p>";
@@ -22,11 +22,12 @@ function populateContent(vendor_data) {
   }
   output += insert_menu;
   var images = vendor_data.images;
+  output += "<div id='images_container'>";
   var image_string = "";
   for(var i = 0; i < images.length; i++) {
     image_string += "<div class='image-container'><img src='../resources/" + images[i].image_url + "' alt='" + images[i].image_url + "' class='images'>";
     image_string += "</div>";
   }
-  output += image_string + "</div>";
+  output += image_string + "</div></div>";
   $("#content").html(output);
 }
