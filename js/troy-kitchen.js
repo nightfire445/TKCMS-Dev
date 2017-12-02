9535a7934b248d188a9c177f878cf480800bb0f7
Yarden <yardenn1@gmail.com>
2017-12-01 19:16:54 -0500

    Removed unused CSS and blocks of commented, unused code

--- js/troy-kitchen.js
+++ js/troy-kitchen.js
@@ -1,13 +1,3 @@
-//code for hover of vendors
-$(document).ready( function() {
-
-//  $('.stall').on('mouseenter', function() {
-//      desc = $(this).attr('data-desc');
-//      $('#content').text(desc.name);
-//  });
-
-});
-
 function populateContent(vendor_data) {
   console.log(vendor_data);
   output = "";
