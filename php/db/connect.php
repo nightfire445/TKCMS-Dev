24f764640bf71f46c0d903f7072b728e668ff88a
Solomon <Moris@rpi.edu>
2017-12-06 13:43:07 -0500

    modifed connect to be conditional, use the JAWSDB_MARIA_URL if accessible but if not use config parameters (that correspond to the hosting

--- php/db/connect.php
+++ php/db/connect.php
@@ -1,22 +1,54 @@
+
 <?php
 $url = getenv('JAWSDB_MARIA_URL');
-$dbparts = parse_url($url);
-$connection_result;
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
-$error;
 
-try {
-    $dbconn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-    // set the PDO error mode to exception
-    $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $connection_result = TRUE;
-    }
-catch(PDOException $e)
-    {
-     $connection_result = FALSE;
-        $error = $e->getMessage();
-    }
+if($url != FALSE){
+
+	$dbparts = parse_url($url);
+	$connection_result;
+	$hostname = $dbparts['host'];
+	$username = $dbparts['user'];
+	$password = $dbparts['pass'];
+	$database = ltrim($dbparts['path'],'/');
+	$error;
+
+
+
+	try {
+	    $dbconn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+	    // set the PDO error mode to exception
+	    $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    $connection_result = TRUE;
+	    }
+	catch(PDOException $e)
+	    {
+	     $connection_result = FALSE;
+	        $error = $e->getMessage();
+	    }
+
+
+
+}
+else{
+	require_once "config.php";
+	$hostname =  $config['DB_HOST'];
+	$database = $config['DB_NAME'];
+	try {
+	    $dbconn = new PDO("mysql:host=$hostname;dbname=$database", $config['DB_USERNAME'], $config['DB_PASSWORD']);
+	    // set the PDO error mode to exception
+	    $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    $connection_result = TRUE;
+	    }
+	catch(PDOException $e)
+	    {
+	     $connection_result = FALSE;
+	        $error = $e->getMessage();
+	    }
+
+}
+
+
+
+
+
 ?>
